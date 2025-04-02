5111a31abac13db62ace91446a62d5605820cf2a
Ryser <rysercooksey67@gmail.com>
2025-04-03 12:19:43 +1300

    My work

--- test.py
+++ test.py
@@ -1,2 +1,36 @@
-print("hello")
-print("htytyryt")
+Name = input("Enter your name: ") 
+
+Age = input("Enter your age") 
+
+ 
+
+print("Choose an activity:") 
+
+print("1. Music Jam Session (2 hours, easy, $5 fee)") 
+
+print("2. Sports Leadership Training (4 hours, moderate $10 fee)") 
+
+print("3. Sports Leadership Training (4 hours, Challenging, $12 fee)") 
+
+Activity = input("Enter the number of your chosen activity: ") 
+
+ 
+
+print("Meal options:") 
+
+print("1. Standard") 
+
+print("2. Vegetarian") 
+
+print("3. Dairy-free") 
+
+print("4. No meal") 
+
+Meal = input("Enter the number of your meal choice: ") 
+
+ 
+
+yn = input(f"{Name}, age {Age}, has chosen {Activity}, Meal option: {Meal}. The total cost is $17. Are you attending (yes/no): ") 
+
+if "yes":
+    print(f"{Name} is confirmed for {Activity}, see you there")
